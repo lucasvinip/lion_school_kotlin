acb4595b0da8d94867d2f881fa1941d0d47e05f0
Lucas Vinicius <110206119+lucasvinip@users.noreply.github.com>
2023-05-23 11:26:35 -0300

    aa

---
+++ app/src/main/java/br/senai/sp/jandira/lionschool/MainActivity.kt
@@ -0,0 +1,79 @@
+package br.senai.sp.jandira.lionschool
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.lionschool.ui.theme.LionSchoolTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LionSchoolTheme {
+                LionSchool()
+            }
+        }
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            3000,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LionSchool() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(51, 71, 176)),
+                verticalArrangement = Arrangement.Center
+        ){
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    modifier = Modifier.size(160.dp),
+                )
+                Text(
+                    text = stringResource(id = R.string.name_school),
+                    modifier = Modifier.width(95.dp),
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            }
+        } // primeira column
+    } // Surface
+}// LionSchool
+
---
+++ app/src/main/java/br/senai/sp/jandira/lionschool/WelcomeActivity.kt
@@ -0,0 +1,40 @@
+package br.senai.sp.jandira.lionschool
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.lionschool.ui.theme.LionSchoolTheme
+
+class WelcomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LionSchoolTheme {
+                WelcomeScreen()
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.background(Color.Blue)
+        ) {
+
+        }
+    }
+}
